4f6677bf3e8e1001ea3c07a220f0b1d16d68f847
AdTekDev <nguyenngtu777@gmail.com>
2023-04-05 10:44:21 +0700

    Add files via upload

---
+++ W0201/js_w02.js
@@ -0,0 +1,11 @@
+
+solan = 0;
+
+alert("Mới chạy lần đầu ! chỉ 1 lần thôi !");
+
+console.log("Mới load xong JS trong trang web !!!");
+
+
+function  tangLen() {
+    document.getElementById("DataThem").innerHTML = "<h1>" + (solan++) + "</h1>";
+}
