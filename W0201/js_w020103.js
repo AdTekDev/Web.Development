4d0f8c99b527dba018db98af7f2a6c8eaa5c3621
AdTekDev <nguyenngtu777@gmail.com>
2023-04-05 11:06:06 +0700

    Add files via upload

---
+++ W0201/js_w020103.js
@@ -0,0 +1,13 @@
+
+solan = 0;
+
+
+function  tangLen() {
+    document.getElementById("DataKetQua").innerHTML 
+    = "<h1>" + (solan++) + "</h1>";
+}
+
+function  giamXuong() {
+    document.getElementById("DataKetQua").innerHTML 
+    = "<h1>" + (solan--) + "</h1>";
+}
